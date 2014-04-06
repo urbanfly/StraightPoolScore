b1a39307c251792b9957d0a551c6fb7fede3c758
Robert Taylor <urbanfly@gmail.com>
2014-04-06 00:54:28 -0400

    reworked the fouls CSS; added "active player" indicator; implemented all of the inning endings

--- StraightPoolScore.Nomad/scripts/controllers.js
+++ StraightPoolScore.Nomad/scripts/controllers.js
@@ -19,8 +19,11 @@ spsControllers.controller("findGameCtrl", ["$scope",
 
 spsControllers.controller("viewGameCtrl", ["$scope", "$routeParams", "$games",
     function ($scope, $routeParams, $games) {
-        var g = $games.load($routeParams.gameId);
-        angular.extend($scope, g);
+        var game = $games.load($routeParams.gameId);
+        angular.extend($scope, game);
+
+        $scope.currentPlayer = (game.currentPlayer == game.player1.id) ? $scope.player1 : $scope.player2;
+        $scope.currentPlayer.isActive = true;
 
         $scope.canSwitch = function () {
             return $scope.scoring.isOpeningRack && $scope.scoring.ballsRemaining == $scope.scoring.maxBalls;
@@ -30,6 +33,8 @@ spsControllers.controller("viewGameCtrl", ["$scope", "$routeParams", "$games",
             var p1 = $scope.player1;
             $scope.player1 = $scope.player2;
             $scope.player2 = p1;
+            $scope.player1.isActive = true;
+            $scope.player2.isActive = false;
         };
 
         $scope.undo = function () {
@@ -38,53 +43,101 @@ spsControllers.controller("viewGameCtrl", ["$scope", "$routeParams", "$games",
 
         $scope.visiblePanel = "scoring";
 
-        $scope.scoring = {
-            inc: function () {
-                if ($scope.scoring.ballsRemaining < $scope.scoring.maxBalls)
-                    $scope.scoring.ballsRemaining++;
-            },
-
-            dec: function () {
-                if ($scope.scoring.ballsRemaining > 1)
-                    $scope.scoring.ballsRemaining--;
-            },
-
-            miss: function () {
-                $scope.scoring.isOpeningRack = false;
-                $scope.scoring.isAfterNewRack = false;
-                $scope.scoring.isAfterBreakingFoul = false;
-            },
-
-            foul: function () {
-                $scope.scoring.isOpeningRack = false;
-                $scope.scoring.isAfterNewRack = false;
-                $scope.scoring.isAfterBreakingFoul = false;
-            },
-
-            safe: function () {
-                $scope.scoring.isOpeningRack = false;
-                $scope.scoring.isAfterNewRack = false;
-                $scope.scoring.isAfterBreakingFoul = false;
-            },
-
-            breakingFoul: function () {
-                $scope.scoring.isAfterBreakingFoul = true;
-                $scope.scoring.isOpeningRack = false;
-            },
-
-            rebreak: function () {
-                $scope.scoring.isAfterBreakingFoul = false;
-                $scope.scoring.isOpeningRack = true;
-            },
-
-            newRack: function () {
-                $scope.scoring.isOpeningRack = false;
-                $scope.scoring.isAfterNewRack = true;
-            },
-
-            with15thBall: function () { },
-            // TODO: How to implement newRack-with-15th-ball
+        $scope.scoring.inc = function () {
+            if ($scope.scoring.ballsRemaining < $scope.scoring.maxBalls)
+                $scope.scoring.ballsRemaining++;
         };
+
+        $scope.scoring.dec = function () {
+            if ($scope.scoring.ballsRemaining > 2)
+                $scope.scoring.ballsRemaining--;
+        };
+
+        $scope.scoring.miss = function () {
+            updateScore();
+            resetFouls();
+            switchPlayers();
+        };
+
+        $scope.scoring.foul = function () {
+            updateScore();
+            var cp = $scope.currentPlayer;
+            cp.fouls++;
+            cp.score--;
+
+            if (cp.fouls == 3)
+            {
+                cp.score -= 15;
+                cp.fouls = 0;
+
+                $scope.scoring.ballsRemaining = 15;
+                $scope.scoring.maxBalls = 15;
+                $scope.scoring.isOpeningRack = true;
+                return;
+            }
+
+            switchPlayers();
+        };
+
+        $scope.scoring.safe = function () {
+            updateScore();
+            resetFouls();
+            switchPlayers();
+        };
+
+        $scope.scoring.breakingFoul = function () {
+            $scope.currentPlayer.score -= 2;
+            $scope.scoring.isAfterBreakingFoul = true;
+            $scope.scoring.isOpeningRack = false;
+            switchPlayers();
+        };
+
+        $scope.scoring.rebreak = function () {
+            $scope.scoring.isAfterBreakingFoul = false;
+            $scope.scoring.isOpeningRack = true;
+            switchPlayers();
+        };
+
+        $scope.scoring.newRack = function () {
+            $scope.scoring.ballsRemaining = 1;
+            updateScore();
+            $scope.scoring.isAfterNewRack = true;
+
+            $scope.scoring.ballsRemaining = 15;
+            $scope.scoring.maxBalls = $scope.scoring.ballsRemaining;
+        };
+
+        $scope.scoring.with15thBall = function () {
+            $scope.scoring.isAfterNewRack = false;
+            $scope.currentPlayer.score++;
+        };
+
+        function updateScore()
+        {
+            var diff = $scope.scoring.maxBalls - $scope.scoring.ballsRemaining;
+            $scope.currentPlayer.score += diff;
+
+            if (diff > 0)
+                resetFouls();
+
+            $scope.scoring.maxBalls = $scope.scoring.ballsRemaining;
+
+            $scope.scoring.isOpeningRack = false;
+            $scope.scoring.isAfterNewRack = false;
+            $scope.scoring.isAfterBreakingFoul = false;
+        }
+
+        function resetFouls()
+        {
+            $scope.currentPlayer.fouls = 0;
+        }
+
+        function switchPlayers()
+        {
+            $scope.currentPlayer.isActive = false;
+            $scope.currentPlayer = ($scope.currentPlayer == $scope.player1) ? $scope.player2 : $scope.player1;
+            $scope.currentPlayer.isActive = true;
+        }
     }
 ]);
 
@@ -106,28 +159,27 @@ spsControllers.controller("profileCtrl", ["$scope",
 
 var spsServices = angular.module("spsServices", []);
 
-spsServices.factory("$games", 
+spsServices.factory("$games",
     function () {
         return {
             load: function (gameId) {
                 return {
                     id: gameId,
                     player1: {
+                        id: "players/1",
                         name: "Robert",
-                        score: 88,
-                        foul1: false,
-                        foul2: false,
-                        foul3: false,
+                        score: 0,
+                        fouls: 0,
                         image: "",
                     },
                     player2: {
+                        id:"players/2",
                         name: "Taylor",
-                        score: 66,
-                        foul1: true,
-                        foul2: true,
-                        foul3: false,
+                        score: 0,
+                        fouls: 0,
                         image: "",
                     },
+                    currentPlayer:"players/1",
                     scoring: {
                         maxBalls: 15,
                         ballsRemaining: 15,
@@ -143,7 +195,15 @@ spsServices.factory("$games",
                         }
                     ],
                     stats: [
-                        { name: "Total Balls", p1: 88, p2: 68 }
+                        { name: "Total Balls", p1: 88, p2: 68 },
+                        { name: "Handicap", p1: 0, p2: 0 },
+                        { name: "High Run", p1: 0, p2: 0 },
+                        { name: "Avg. Run", p1: 0, p2: 0 },
+                        { name: "Total Fouls", p1: 0, p2: 0 },
+                        { name: "Total Safes", p1: 0, p2: 0 },
+                        { name: "Successful Safes", p1: 0, p2: 0 },
+                        { name: "Balls b/w Errorrs", p1: 0, p2: 0 },
+                        { name: "StdDev Run", p1: 0, p2: 0 },
                     ]
                 };
             }
